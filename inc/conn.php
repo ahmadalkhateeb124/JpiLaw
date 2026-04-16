5d5a702a5480a013bd0f5a5ed38e8a3e441f02ff
ahmad alkhatib <ahmadalkhatibwork@yahoo.com>
2026-04-16 17:18:33 +0300

    Edait File Htaccess And Conn.Php

--- inc/conn.php
+++ inc/conn.php
@@ -11,29 +11,10 @@ if (strpos($url, 'localhost') !== false || strpos($url, '127.0.0.1') !== false) 
     $base_path = $_SERVER['DOCUMENT_ROOT'] . '/JpiLaw/';
 } else {
 
-    // $conn = mysqli_connect("localhost", "u366180362_k14ncom", "KayanTechCo@2002#", "u366180362_KayanTechCo");
     $base_url = "https://jpilawfirm.com/";
 
     $base_path = $_SERVER['DOCUMENT_ROOT'];
 }
-
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-
-
-// $web_info = [];
-// $sel_info = "select * from SystemInfo where Lang='$lang'";
-// $res = mysqli_query($conn, $sel_info);
-// while ($data = mysqli_fetch_assoc($res)) {
-//     $web_info[] = $data;
-// }
-
-
-// $currentPageURL = $_SERVER['REQUEST_URI'];
-// $currentPageName = basename($currentPageURL, ".php");
-// echo "Opened page name: " . $currentPageName;
-
 
 function getCurrentURL()
 {
@@ -50,7 +31,3 @@ function getCurrentURL()
 // Usage example:
 $currentURL = getCurrentURL();
 
-
-// include_once($base_path . 'inc/UpdateVisitCount.php');
-// include_once($base_path . 'inc/UpdateVisitCount.php');
-// include_once($base_path . 'inc/get_info.php');
